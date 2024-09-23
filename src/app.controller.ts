8b37635ff38f73e50b2a1789eade44338fbd725f
Oláh András <106300817+oandris228@users.noreply.github.com>
2024-09-23 14:05:32 +0200

    error

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Param, Query, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { title } from 'process';
+import { query } from 'express';
 
 @Controller()
 export class AppController {
@@ -7,9 +9,48 @@ export class AppController {
 
   @Get()
   @Render('index')
-  getHello() {
+  getHello(@Query('bgColor') bgColor: string = 'aqua') {
     return {
+      bgColor: bgColor,
       message: this.appService.getHello()
     };
+  }
+
+  #books = [
+    {
+      title: "The lord",
+      isbn: '1'
+    },
+    {
+      title: "The art",
+      isbn: '2'
+    },
+    {
+      title: "The ball",
+      isbn: '3'
+    },
+  ]
+
+  @Get('booklist')
+  @Render('books')
+  getBooks() {
+    return {
+      books: this.#books
+    }
+  }
+
+  @Get('books')
+  @Render('booklist')
+  getAllBooks(@Query('title') title: string) {
+    return {
+    };
+  }
+
+  @Get('books/:isbn')
+  @Render('book')
+  getBookByIsbn(@Param('isbn') isbn: string) {
+    return this.#books.find(
+      b => b.isbn == isbn
+    )
   }
 }
